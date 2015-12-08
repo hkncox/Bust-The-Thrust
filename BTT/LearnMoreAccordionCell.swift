dfc3c30b1be6dc4bce56009009ddcb35f2673946
hkncox <hkncox@gmail.com>
2015-12-07 17:17:11 -0700

    I Changed the learn more page

---
+++ BTT/LearnMoreAccordionCell.swift
@@ -0,0 +1,11 @@
+//
+//  LearnMoreAccordionCell.swift
+//  BTT
+//
+//  Created by Nicole Talley on 12/7/15.
+//  Copyright © 2015 Nicole Talley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
